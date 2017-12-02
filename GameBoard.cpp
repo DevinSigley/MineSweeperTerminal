dbf93258c9fc3f464e5864bc6f890accd58cb635
DevinSigley <devinsigley@gmail.com>
2017-12-01 22:34:27 -0500

    Added checkSurrounding function

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -53,26 +53,165 @@ void GameBoard::initializeBoard(int default) {
 	}
 }
 
-// Checks how many bombs are nearby; returns it as an int.
+// Checks how many bombs are nearby; returns it as an int. Returns -1 if selected is bomb.
 int GameBoard::checkSurrounding(int x, int y) {
 	int bombSum = 0;
 
-	// Checking topleft and bottomleft corners
+	// Return -1 if selected space is a bomb.
+	if (board[x][y] == BOMB) {
+		return -1;
+	}
+
+	// Checking left side
 	if (x == 0) {
 		if (y == 0) {
-			if (board[x][y+1] == BOMB) {
+			if (board[x][y + 1] == BOMB) {
 				bombSum++;
 			}
-			if (board[x+1][y] == BOMB) {
+			if (board[x + 1][y] == BOMB) {
 				bombSum++;
 			}
-			if (board[x+1][y+1] == BOMB) {
+			if (board[x + 1][y + 1] == BOMB) {
 				bombSum++;
 			}
 		}
 		else if (y == size) {
-
+			if (board[x][y - 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x + 1][y] == BOMB) {
+				bombSum++;
+			}
+			if (board[x + 1][y - 1] == BOMB) {
+				bombSum++;
+			}
+		}
+		else {
+			if (board[x][y - 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x][y + 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x + 1][y - 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x + 1][y] == BOMB) {
+				bombSum++;
+			}
+			if (board[x + 1][y + 1] == BOMB) {
+				bombSum++;
+			}
 		}
 	}
+
+	// Checking right side
+	else if (x == size) {
+		if (y == 0) {
+			if (board[x][y + 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x - 1][y] == BOMB) {
+				bombSum++;
+			}
+			if (board[x - 1][y + 1] == BOMB) {
+				bombSum++;
+			}
+		}
+		else if (y == size) {
+			if (board[x][y - 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x - 1][y] == BOMB) {
+				bombSum++;
+			}
+			if (board[x - 1][y - 1] == BOMB) {
+				bombSum++;
+			}
+		}
+		else {
+			if (board[x][y - 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x][y + 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x - 1][y - 1] == BOMB) {
+				bombSum++;
+			}
+			if (board[x - 1][y] == BOMB) {
+				bombSum++;
+			}
+			if (board[x - 1][y + 1] == BOMB) {
+				bombSum++;
+			}
+		}
+	}
+
+	// Checking top
+	if (y == 0) {
+		// Top corners already checked by preceding x checks, so excluded.
+		if (board[x - 1][y] == BOMB) {
+			bombSum++;
+		}
+		if (board[x + 1][y] == BOMB) {
+			bombSum++;
+		}
+		if (board[x][y + 1] == BOMB) {
+			bombSum++;
+		}
+		if (board[x - 1][y + 1] == BOMB) {
+			bombSum++;
+		}
+		if (board[x + 1][y + 1] == BOMB) {
+			bombSum++;
+		}
+	}
+
+	// Checking bottom
+	else if (y == size) {
+		if (board[x - 1][y] == BOMB) {
+			bombSum++;
+		}
+		if (board[x + 1][y] == BOMB) {
+			bombSum++;
+		}
+		if (board[x][y - 1] == BOMB) {
+			bombSum++;
+		}
+		if (board[x - 1][y - 1] == BOMB) {
+			bombSum++;
+		}
+		if (board[x + 1][y - 1] == BOMB) {
+			bombSum++;
+		}
+	}
+	
+	// Checking space not on edge of board
+	if (board[x - 1][y] == BOMB) {
+		bombSum++;
+	}
+	if (board[x + 1][y] == BOMB) {
+		bombSum++;
+	}
+	if (board[x][y - 1] == BOMB) {
+		bombSum++;
+	}
+	if (board[x][y + 1] == BOMB) {
+		bombSum++;
+	}
+	if (board[x - 1][y - 1] == BOMB) {
+		bombSum++;
+	}
+	if (board[x - 1][y + 1] == BOMB) {
+		bombSum++;
+	}
+	if (board[x + 1][y - 1] == BOMB) {
+		bombSum++;
+	}
+	if (board[x + 1][y + 1] == BOMB) {
+		bombSum++;
+	}
+
 	return bombSum;
 }
